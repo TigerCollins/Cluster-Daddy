6f380083dac50edd9593c216b67ec544e843fe2a
HomicidalNacho <tiger.collins@outlook.com>
2016-10-27 01:09:58 +1000

    First Update.
    COMPLETED
    - Implemented Main Menu.
    - All predicted required assets aquired.
    - Basic layout of Level 1 complete.
    - Implemented all Main Menu sections
    
    YET TO IMPLEMENT
    - Audio.
    - Working conveyer belts.
    - All menus implemented.
    - Physics modifiers.
    - UI death count.

---
+++ Assets/Scripts/LevelUI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+
+public class LevelUI : MonoBehaviour {
+    //Timer
+    public Text Timer;
+    public float TimerValue= 0f;
+
+	//Use this for initialization.
+	void Start ()
+    {
+	
+	}
+
+    //Called once per frame.
+    void Update()
+    {
+               
+    }
+
+    //Called according to timescale.
+    void FixedUpdate()
+    {
+        TimerFunction();
+    }
+
+    //Sets the Timer.
+    void TimerFunction()
+    {
+        TimerValue += Time.deltaTime;
+        Timer.text = TimerValue.ToString("f2");
+    }
+    
+}
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+    void Start()
+    {
+
+    }
+
+    //Called once per frame.
+    void Update()
+    {
+
+    }
+
+    //Called according to timescale.
+    void FixedUpdate()
+    {
+
+    }
+
+    //Tests if the button works.
+    public void TestClick()
+    {
+        Debug.Log("Button Pressed.");
+    }
+
+    //Loads Main Menu.
+    public void MainMenu()
+    {
+        Application.LoadLevel("Main Menu");
+    }
+
+    //Quits to Desktop.
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Application Quit. Only works in packaged version.");
+    }
+
+    //Opens Credits.
+    public void Credits()
+    {
+        Application.LoadLevel("Credits");
+    }
+
+    //Opens Level 1.
+    public void L1()
+    {
+        Application.LoadLevel("Level1");
+    }
+
+    //Opens Level 2.
+    public void L2()
+    {
+        Debug.Log("Level 2.");
+    }
+
+    //Opens Level 3.
+    public void L3()
+    {
+        Debug.Log("Level 3.");
+    }
+}
